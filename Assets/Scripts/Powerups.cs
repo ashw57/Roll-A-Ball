773c4dd06ca945a222ec497aae0ee10f650a62c3
ashw57 <nugoot57@gmail.com>
2024-04-22 11:39:56 +1000

    Powerup coding continued

--- Assets/Scripts/Powerups.cs
+++ Assets/Scripts/Powerups.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Powerups : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   //An enum is a datatype taht we can specify its values and use
+   public enum PowerupType {SpeedUp, SpeedDown}
+
+    public PowerupType myPowerup;              //This objects powerup type
+    public float powerupDuration = 5.0f;       //The duration of the powerup
+    PlayerController playerController;         //A reference to our player controller
+
+     void Start()
     {
-        
+        //Find and assign the player conroller object to this local reference
+        playerController = FindObjectOfType<PlayerController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UsePowerUp()
     {
-        
+        //if this powerup is a speedup powerup, increase the players speed by double
+        if (myPowerup == PowerupType.SpeedUp)
+            playerController.speed = playerController,baseSpeed * 2;
+
+        //if this powerip is the speedDown powerup, decrease the player controller speed times 3
+        if (myPowerup == )
     }
 }
